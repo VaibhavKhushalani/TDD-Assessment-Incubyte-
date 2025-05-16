1dd6e0a5b803d1bd9534c056a4f45eb0eb68540a
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2025-05-16 07:47:49 +0530

    Support newline character as a delimiter

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   if (!numbers) return 0;
-  const nums = numbers.split(",");
+  const nums = numbers.replace(/\n/g, ",").split(",");
   return nums.reduce((sum, n) => sum + parseInt(n, 10), 0);
 }
 
-console.assert(add("1,2") === 3, "Sum of two numbers");
+console.assert(add("1\n2,3") === 6, "Handles new lines as delimiters");
