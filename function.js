49a8bec1bdd4db08d55f9b9d732f79897eac69a6
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2025-05-16 07:48:20 +0530

    Support custom delimiter specified at the start

--- function.js
+++ function.js
@@ -1,7 +1,14 @@
 function add(numbers) {
   if (!numbers) return 0;
-  const nums = numbers.replace(/\n/g, ",").split(",");
+
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    delimiter = numbers.charAt(2);
+    numbers = numbers.slice(4);
+  }
+
+  const nums = numbers.replace(/\n/g, delimiter).split(delimiter);
   return nums.reduce((sum, n) => sum + parseInt(n, 10), 0);
 }
 
-console.assert(add("1\n2,3") === 6, "Handles new lines as delimiters");
+console.assert(add("//;\n1;2") === 3, 'Custom delimiter ";"');
